ba4e9c4562448ac7b984caf5cee6f970297610e0
LukasJCode <89071785+LukasJCode@users.noreply.github.com>
2021-11-03 12:17:48 +0100

    Revert "Health checks added"
    This reverts commit bb91dab29fbf46bc3ed2e48790bcf4fbf553bf23.

--- Portfolio/Startup.cs
+++ Portfolio/Startup.cs
@@ -1,13 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Mime;
-using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -38,11 +34,11 @@ namespace Portfolio
         {
             BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
             BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
-            var mongoDbSettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
 
             services.AddSingleton<IMongoClient>(ServiceProvider => 
             {
-                            return new MongoClient(mongoDbSettings.ConnectionString);
+                var settings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+                return new MongoClient(settings.ConnectionString);
             });
 
             services.AddSingleton<IProjectsRepository, MongoDbProjectsRepository>();
@@ -55,13 +51,6 @@ namespace Portfolio
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Portfolio", Version = "v1" });
             });
-
-            services.AddHealthChecks()
-                .AddMongoDb(
-                    mongoDbSettings.ConnectionString, 
-                    name: "mongodb", 
-                    timeout: TimeSpan.FromSeconds(3),
-                    tags:new[]{"ready"});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -83,32 +72,6 @@ namespace Portfolio
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                 endpoints.MapHealthChecks("/health/ready", new HealthCheckOptions
-                {
-                    Predicate = (check) => check.Tags.Contains("ready"),
-                    ResponseWriter = async(context, report) =>
-                    {
-                        var result = JsonSerializer.Serialize
-                        (
-                            new
-                            {
-                                status = report.Status.ToString(),
-                                checks = report.Entries.Select(entry => new{
-                                    name = entry.Key,
-                                    status = entry.Value.Status.ToString(),
-                                    exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
-                                    duration = entry.Value.Duration.ToString()
-                                })
-                            }
-                        );
-                        context.Response.ContentType= MediaTypeNames.Application.Json;
-                        await context.Response.WriteAsync(result);
-                    }
-                });
-
-                endpoints.MapHealthChecks("/health/live", new HealthCheckOptions{
-                    Predicate = (_) => false
-                });
             });
         }
     }
