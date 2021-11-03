c473214a67e29849b8914ea6e100c92dc8d3e15c
LukasJCode <89071785+LukasJCode@users.noreply.github.com>
2021-11-03 13:01:29 +0100

    docker setup

--- Portfolio/Startup.cs
+++ Portfolio/Startup.cs
@@ -62,9 +62,11 @@ namespace Portfolio
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Portfolio v1"));
             }
-
-            app.UseHttpsRedirection();
-
+            if(env.IsDevelopment())
+            {
+                app.UseHttpsRedirection();
+            }
+            
             app.UseRouting();
 
             app.UseAuthorization();
